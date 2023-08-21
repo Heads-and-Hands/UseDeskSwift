26c1f142e35f1f0d19bd99bcaa5d528133b15591
zlata.samarskaya <zsamarskaya@esn.team>
2023-08-21 17:44:14 +0300

    socket.io ver fix

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package (
         .library(name: "UseDesk", targets: ["UseDesk"])
     ],
     dependencies: [
-        .package(name: "SocketIO", url: "https://github.com/socketio/socket.io-client-swift", from: "16.0.0"),
+        .package(name: "SocketIO", url: "https://github.com/socketio/socket.io-client-swift", exact: "16.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         .package(url: "https://github.com/sendyhalim/Swime", from: "3.0.0"),
         .package(url: "https://github.com/johnxnguyen/Down", from: "0.11.0")
